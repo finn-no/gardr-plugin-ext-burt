4a23e9c1a5c9fae6ea766f052dc018c510a2bf6e
micgunnu <michael.gunnulfsen@finn.no>
2014-06-02 08:01:45 +0200

    Added option burtConnect to connect a burtUnit with a campaign

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var win = global, d = document, gardrParams;
+var eventListener = require('eventlistener');
 var validateOpts = require('define-options')({
-    burtScript       : 'string    - url to the burt xdi-script'
+    burtScript       : 'string    - Required URL to the burt xdi-script',
+    burtConnect      : '?|function  - Optional function to connect campaignId to a burtUnit. Arguments (burtUnit, containerEl).'
 });
 
 function saveParams (params) {
     gardrParams = params;
 }
 
-function trackGardrContainer (el) {
-    if ( win.burtApi.trackById ) {
+function trackGardrContainer (options, el) {
+    if (win.burtApi.trackByNode) {
         el.setAttribute('data-name', gardrParams.id);
         el.setAttribute('data-xdi-id', gardrParams.id);
-        win.burtApi.trackById(el.id);
+        burtUnit = win.burtApi.trackByNode(el, {name : gardrParams.id, xdiId : gardrParams.id});
+        if (options.burtConnect) {
+            options.burtConnect(burtUnit, el);
+        }
     } else {
         win.burtApi.push(trackGardrContainer.bind(null, el));
     }
@@ -20,7 +25,7 @@ function trackGardrContainer (el) {
 function burtExt (gardrPluginApi, options) {
     win.burtApi = win.burtApi || [];
     gardrPluginApi.on('params:parsed', saveParams);
-    gardrPluginApi.on('element:containercreated', trackGardrContainer);
+    gardrPluginApi.on('element:containercreated', trackGardrContainer.bind(null,options));
 
     var s = d.createElement('script');
     s.src = options.burtScript;
--- index.test.js
+++ index.test.js
@@ -1,9 +1,12 @@
-var burtExt = require('./index.js');
-var PluginApi = require('gardr-core-plugin').PluginApi;
+var burtExt = require('./index.js'),
+    win = global,
+    PluginApi = require('gardr-core-plugin').PluginApi;
 
 function mockBurtScript () {
     var burtApi = global.burtApi;
-    burtApi.trackByNode = sinon.spy();
+    burtApi.trackByNode = sinon.spy(function() {
+        return 'burtUnitMock';
+    });
     burtApi.trackById = sinon.spy();
     if (burtApi.length > 0) {
         burtApi.shift()();
@@ -13,6 +16,7 @@ function mockBurtScript () {
 function createContainer () {
     var c = document.createElement('span');
     c.id = 'gardr';
+
     return c;
 }
 
@@ -22,8 +26,8 @@ describe('burt-ext', function () {
     };
     var pluginApi;
 
-    options.without = function without (removeKey) {
-        var obj = {};
+    options.modify = function modify (removeKey, extendObj) {
+        var obj = extendObj || {};
         for (var key in this) {
             if (key !== removeKey) { obj[key] = this[key]; }
         }
@@ -47,7 +51,7 @@ describe('burt-ext', function () {
 
     it('should throw if missing burtScript options argument', function () {
         expect(function () {
-            burtExt(null, options.without('burtScript'));
+            burtExt(null, options.modify('burtScript'));
         }).to.throw();
     });
 
@@ -66,37 +70,77 @@ describe('burt-ext', function () {
             spy.restore();
         });
 
-        it('should call burtApi.trackById if burtScript loads before container is created', function () {
+        it('should call burtApi.trackByNode if burtScript loads before container is created', function () {
             burtExt(pluginApi, options);
 
             mockBurtScript();
+            var container = createContainer();
             pluginApi.trigger('params:parsed', {id: 'test1'});
-            pluginApi.trigger('element:containercreated', createContainer());
-            expect(burtApi.trackById).to.have.been.calledOnce;
-            expect(burtApi.trackById).to.have.been.calledWith('gardr');
+            pluginApi.trigger('element:containercreated', container);
+            expect(burtApi.trackByNode).to.have.been.calledOnce;
+            expect(burtApi.trackByNode).to.have.been.calledWith(container);
+            //expect(burtApi.trackByNode).to.have.been.calledWith(container, {name : 'test1', xdiId : 'text1'});
         });
 
-        it('should call burtApi.trackById if burtScript loads after container is created', function () {
+        it('should call burtApi.trackByNode if burtScript loads after container is created', function () {
             burtExt(pluginApi, options);
-
-            pluginApi.trigger('params:parsed', {id: 'test2'});
-            pluginApi.trigger('element:containercreated', createContainer());
             mockBurtScript();
-            expect(burtApi.trackById).to.have.been.calledOnce;
-            expect(burtApi.trackById).to.have.been.calledWith('gardr');
+            var container = createContainer();
+            pluginApi.trigger('params:parsed', {id: 'test2'});
+            pluginApi.trigger('element:containercreated', container);
+            expect(burtApi.trackByNode).to.have.been.calledOnce;
+            expect(burtApi.trackByNode).to.have.been.calledWith(container);
+        });
+
+        it('should call burtApi.trackByNode with proper arguments', function () {
+            var burtPlugin = burtExt(pluginApi, options);
+            mockBurtScript();
+            var gardrId = 'foo';
+
+            var container = createContainer();
+            pluginApi.trigger('params:parsed', {id: gardrId});
+            pluginApi.trigger('element:containercreated', container);
+            expect(burtApi.trackByNode).to.have.been.calledWithExactly(container, { name : gardrId, xdiId : gardrId });
         });
 
         it('should set data-name and data-xdi-id on container', function () {
             burtExt(pluginApi, options);
-            var params = {id: 'test3'};
+            mockBurtScript();
+            var params = {id: 'test4'};
+
             pluginApi.trigger('params:parsed', params);
 
             var container = createContainer();
             pluginApi.trigger('element:containercreated', container);
-            mockBurtScript();
 
             expect(container.getAttribute('data-name')).to.equal(params.id);
             expect(container.getAttribute('data-xdi-id')).to.equal(params.id);
+        });
+
+        it('should not throw if burtConnect not specified in options', function () {
+            expect(function () {
+                var burtPlugin = burtExt(pluginApi, options.modify('burtConnect'));
+                mockBurtScript();
+
+                pluginApi.trigger('params:parsed', {id: 'footest'});
+                pluginApi.trigger('element:containercreated', createContainer());
+            }).not.to.throw();
+        });
+
+        it('should call burtConnect after container created', function () {
+            var spy = sinon.spy();
+            var burtPlugin = burtExt(pluginApi, options.modify(null, {
+                burtConnect: spy
+            }));
+            mockBurtScript();
+
+            var container = createContainer();
+
+            pluginApi.trigger('params:parsed', {id: 'footest'});
+            pluginApi.trigger('element:containercreated', container);
+
+            expect(spy).to.have.been.calledOnce;
+            expect(spy).to.have.been.calledWithExactly('burtUnitMock', container);
         });
     });
 });
